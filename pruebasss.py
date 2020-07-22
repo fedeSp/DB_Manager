e2990bc4529529b4c32257dc8914fef5f528400d
fedeSp <53310545+fedeSp@users.noreply.github.com>
2020-07-21 21:50:43 -0300

    Delete pruebasss.py

--- pruebasss.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import pandas as pd
-
-with sqlite3.connect('C:/Users/Fede/source/repos/CRUD/database.db') as conn:
-    conn.row_factory = sqlite3.Row
-    query = 'SELECT * FROM {}'.format("product")
-    df = pd.read_sql(query,conn)
-    print(df)
